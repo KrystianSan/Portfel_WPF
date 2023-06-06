5e491ca91dbca0259ef87a6660f527ed0504e19c
KrystianSan <krystian122000@wp.pl>
2023-06-06 23:23:32 +0200

    start

--- Portfel/MainWindow.xaml.cs
+++ Portfel/MainWindow.xaml.cs
@@ -15,14 +15,55 @@ using System.Windows.Shapes;
 
 namespace Portfel
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private decimal walletBalance;
+
         public MainWindow()
         {
             InitializeComponent();
+            walletBalance = 0;
+            UpdateBalanceText();
+        }
+
+        private void UpdateBalanceText()
+        {
+            balanceText.Text = walletBalance.ToString("C"); // Display balance as currency
+        }
+
+        private void AddMoney_Click(object sender, RoutedEventArgs e)
+        {
+            // Get transaction name from the TextBox
+            string transactionName = transactionNameTextBox.Text;
+            string boxBalance = balance;
+            // Simulate adding money to the wallet
+            walletBalance += ;
+            UpdateBalanceText();
+
+            // Clear the TextBox after adding money
+            transactionNameTextBox.Clear();
+
+            // Display a message box with the transaction details
+            MessageBox.Show($"Added $10 to the wallet.\nTransaction Name: {transactionName}");
+        }
+
+        private void RemoveMoney_Click(object sender, RoutedEventArgs e)
+        {
+            // Get transaction name from the TextBox
+            string transactionName = transactionNameTextBox.Text;
+
+            // Simulate removing money from the wallet
+            if (walletBalance >= 10)
+            {
+                walletBalance -= 10;
+                UpdateBalanceText();
+
+                // Clear the TextBox after removing money
+                transactionNameTextBox.Clear();
+
+                // Display a message box with the transaction details
+                MessageBox.Show($"Removed $10 from the wallet.\nTransaction Name: {transactionName}");
+            }
         }
     }
 }
