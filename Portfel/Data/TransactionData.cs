56a7dfc718e155b5e2fa73caaa4aa242450973a8
remkeah <gmasiewicz@gmail.com>
2023-06-17 03:00:04 +0200

    commit 3

---
+++ Portfel/Data/TransactionData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfel.Data
+{
+    public static class TransactionData
+    {
+        public static void AddTransactionToDB(Transaction transaction)
+        {
+            using(var db = new TransactionContext())
+            {
+                db.Add(transaction);
+                db.SaveChanges();
+            }
+        }
+
+        public static List<Transaction> GetTransactionList()
+        {
+            using(var db = new TransactionContext())
+            {
+                return db.Transactions.ToList();
+            }
+        }
+    }
+}
